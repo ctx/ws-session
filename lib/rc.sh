40dc8d48a4b9b4f267814e963a1f3fcf2c518eef
ctx <ctroxler@gmail.com>
2013-10-28 00:38:58 +0100

    rename rc file

--- lib/rc.sh
+++ lib/rc.sh
@@ -1,10 +1,10 @@
-if [[ -f $HOME/.session.rc ]] ; then
-  source $HOME/.session.rc
-elif [[ -f $HOME/.config/ws-sesson/session.rc ]] ; then
-  source $HOME/.config/ws-sesson/session.rc
+if [[ -f $HOME/.ws-session.rc ]] ; then
+  source $HOME/.ws-session.rc
+elif [[ -f $HOME/.config/ws-sesson/ws-session.rc ]] ; then
+  source $HOME/.config/ws-sesson/ws-session.rc
 else
   echo Error session.rc not found!
-  echo install it to $HOME/.session.rc or $XDG_CONFIG_HOME/session.rc
+  echo install it to $HOME/.ws-session.rc or $XDG_CONFIG_HOME/ws-session.rc
 fi
 
 # vim: ft=sh ts=2 et sw=2:
