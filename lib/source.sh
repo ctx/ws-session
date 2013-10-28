e2da2962ecf5a376df3ed3936cc19bf91be13190
ctx <ctroxler@gmail.com>
2013-10-29 00:53:31 +0100

    more cleanup

--- lib/source.sh
+++ lib/source.sh
@@ -1,17 +1,17 @@
 s_source_rc() {
   if [[ -f $HOME/.ws-session.rc ]] ; then
     source $HOME/.ws-session.rc
-  elif [[ -f $HOME/.config/ws-sesson/ws-session.rc ]] ; then
-    source $HOME/.config/ws-sesson/ws-session.rc
+  elif [[ -f $HOME/.config/ws-session/ws-session.rc ]] ; then
+    source $HOME/.config/ws-session/ws-session.rc
   else
     echo Error session.rc not found!
-    echo install it to $HOME/.ws-session.rc or $S_CONFIGFOLDER/ws-session.rc
+    echo install it to $HOME/.ws-session.rc or $XDG_COFIG_HOME/ws-session/ws-session.rc
   fi
 }
 
 s_source() {
-  if [[ -f "$S_CONFIGFOLDER/$1" ]] ; then
-    source "$S_CONFIGFOLDER/$1"
+  if [[ -f "$S_CONFIG_FOLDER/$1" ]] ; then
+    source "$S_CONFIG_FOLDER/$1"
   elif [[ -f "$S_ROOT_FOLDER/$1" ]] ; then
     source "$S_ROOT_FOLDER/$1"
   else
