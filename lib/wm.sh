2e46941bc58df42ecc86100695fa0bb6892d53ea
ctx <ctroxler@gmail.com>
2013-10-28 23:27:47 +0100

    move wm folder

--- lib/wm.sh
+++ lib/wm.sh
@@ -1,18 +1,18 @@
 if [[ $(wmiir ls / 2>/dev/null) ]] ; then
   WM=wmii
-  source $SESSIONPATH/lib/wm/wmii.sh
+  source $SESSIONPATH/wm/wmii.sh
 elif [[ $(wmctrl -m | grep llwm) ]]; then
   WM=llwm
-  source $SESSIONPATH/lib/wm/llwm.sh
+  source $SESSIONPATH/wm/llwm.sh
 elif [[ $(i3-msg -t get_version 2>/dev/null) ]]; then
   WM=i3
-  source $SESSIONPATH/lib/wm/i3.sh
+  source $SESSIONPATH/wm/i3.sh
 elif [[ $(wmctrl -m | grep bspwm) ]] ; then
   WM=bspwm
-  source $SESSIONPATH/lib/wm/bspwm.sh
+  source $SESSIONPATH/wm/bspwm.sh
 elif [[ $(wmctrl -m | grep Openbox) ]]; then
   WM=ob
-  source $SESSIONPATH/lib/wm/ob.sh
+  source $SESSIONPATH/wm/ob.sh
 else
   WM=NULL
 fi
