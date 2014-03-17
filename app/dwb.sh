f420e55ae001138831a890154e85d49273ba2e53
ctx <ctroxler@gmail.com>
2014-03-17 23:12:03 +0100

    shorter

--- app/dwb.sh
+++ app/dwb.sh
@@ -3,80 +3,44 @@ dwbcmd='/usr/bin/dwb -n'
 dwbcmdreload="$dwbcmd -r default"
 
 # open a dwb session
-# arg1: Data folder: where the last session was stored.
-# Copy the dwb data folder arg1/dwb to arg2
+# $1: Data folder: where the last session was stored.
+# Copy the dwb data folder arg1/dwb to tmp_dir
 s_dwb_open_session() {
-  local dir="$1"
-  if [[ -d "$dir/$DWB" ]] ; then
+  if [[ -d "$1/$DWB" ]] ; then
     if ! [[  -d "$tmp_dir/$DWB" ]] ; then
-      cp -R "$dir/$DWB" "$tmp_dir"
+      cp -R "$1/$DWB" "$tmp_dir"
     fi
-
-    xch="$XDG_CONFIG_HOME"
-    XDG_CONFIG_HOME="$tmp_dir"
-    $dwbcmdreload & >/dev/null 2>&1
-    pid="$!"
-    s_reg_winid $pid "$(cat "$tmp_dir/$DWB/windowid")"
-    XDG_CONFIG_HOME="$xch"
-    unset xch pid
+    XDG_CONFIG_HOME="$tmp_dir" $dwbcmdreload & >/dev/null 2>&1
+    s_reg_winid $! "$(cat "$tmp_dir/$DWB/windowid")"
+    unset pid
   fi
 }
 
 # close dwb session
-# arg1: windowid
+# $1: windowid
 s_dwb_close_session() {
   echo $1 > "$tmp_dir/$DWB/windowid"
-  xch="$XDG_CONFIG_HOME"
-  XDG_CONFIG_HOME="$tmp_dir/$DWB"
-  dwb -x save_session
-  XDG_CONFIG_HOME="$xch"
-  unset xch
-  #s_focus_window $1
-  #xdotool key KP_Escape
-  #xdotool type ZZ
-}
-
-s_dwb_focus() {
-  s_focus_window "$1"
-  if [[ -n $2 ]] ; then 
-    xdotool type "O$2"
-    xdotool key KP_Enter
-  fi
+  # XDG_CONFIG_HOME="$tmp_dir" /usr/bin/dwbremote -i $1 hook quit
+  s_focus_window $1
+  xdotool key KP_Escape
+  xdotool type ZZ
 }
 
 s_dwb_start() {
-  local tmp_dir="$S_TEMP_FOLDER/$S_SEL_TAG"
+  local tmp_dir="$S_TEMP_FOLDER/$S_SEL_TAG" 
   if ! [[ -d $tmp_dir/$DWB ]] ; then
     mkdir "$tmp_dir/$DWB"
-    for f in $(find "$XDG_CONFIG_HOME/$DWB" -maxdepth 1 -not -name default) ; do
-      [[ $f != $XDG_CONFIG_HOME/$DWB ]] && ln -s "$f" "$tmp_dir/$DWB/$(basename "$f")"
-    done
-    xch="$XDG_CONFIG_HOME"
-    XDG_CONFIG_HOME="$tmp_dir"
-    $dwbcmd "$@" & >/dev/null 2>&1
-    XDG_CONFIG_HOME="$xch"
-    unset xch
+    find "$XDG_CONFIG_HOME/$DWB" -maxdepth 1 -mindepth 1 -not -name default \
+      -exec ln -s "{}" "$tmp_dir/$DWB/" \;
+    XDG_CONFIG_HOME="$tmp_dir" $dwbcmd "$@" & >/dev/null 2>&1
   else
     winid="$(s_list_app_seltag | grep $DWB | cut -f1)"
     if [[ -n $winid ]] ; then
       s_dwb_focus "$winid" "$@"
     else
-      xch="$XDG_CONFIG_HOME"
-      XDG_CONFIG_HOME="$tmp_dir"
-      $dwbcmd "$@" & >/dev/null 2>&1
-      XDG_CONFIG_HOME="$xch"
-      unset xch
+      XDG_CONFIG_HOME="$tmp_dir" $dwbcmd "$@" & >/dev/null 2>&1
     fi
   fi
-}
-
-s_dwb_orig() {
-  local tmp_dir="$S_TEMP_FOLDER/$S_SEL_TAG/$DWB"
-  xch="$XDG_CONFIG_HOME"
-  XDG_CONFIG_HOME="$tmp_dir"
-  $dwbcmd "$@" & >/dev/null 2>&1
-  XDG_CONFIG_HOME="$xch"
-  unset xch
 }
 
 # vim: ft=sh ts=2 et sw=2:
