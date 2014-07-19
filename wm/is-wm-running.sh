9bb2491294087a68399d775bf5572f3c095ee645
ctx <ctroxler@gmail.com>
2014-07-19 16:45:59 +0200

    wm: fix not installed wms

--- wm/is-wm-running.sh
+++ wm/is-wm-running.sh
@@ -1,19 +1,33 @@
 # Add your own to this list
 
 s_running_wm_wmii() {
-  wmiir ls / 2>/dev/null
+  if which wmiir > /dev/null 2>&1; then
+    wmiir ls / 2> /dev/null
+  fi
 }
 
 s_running_wm_i3() {
-  i3-msg -t get_version 2>/dev/null
+  if which i3-msg > /dev/null 2>&1; then
+    i3-msg -t get_version 2> /dev/null
+  fi
+}
+
+s_running_wm_default() {
+  if which wmctrl > /dev/null 2>&1; then
+    wmctrl -m | grep "$@"
+  fi
 }
 
 s_running_wm_bspwm() {
-  wmctrl -m | grep bspwm
+  s_running_wm_default bspwm
 }
 
 s_running_wm_herbstluftwm() {
-  wmctrl -m | grep herbstluftwm
+  s_running_wm_default herbstluftwm
+}
+
+s_running_wm_llwm() {
+  s_running_wm_default llwm
 }
 
 # vim: ft=sh ts=2 et sw=2:
